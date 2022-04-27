6a4c10f872a7f8fa5bd0617fc5cf2c8a0dc012ea
johnnyZeppelin <936645698@qq.com>
2022-04-27 22:10:23 +0800

    Add class OffByOne and class OffByN and finish the commanded tasks

--- proj1b/CharacterComparator.java
+++ proj1b/CharacterComparator.java
@@ -3,3 +3,22 @@ public interface CharacterComparator {
     /** Returns true if characters are equal by the rules of the implementing class. */
     boolean equalChars(char x, char y);
 }
+
+class OffByOne implements CharacterComparator {
+    @Override
+    public boolean equalChars(char x, char y) {
+        return x - y == 1 || y - x == 1;
+    }
+}
+
+class OffByN implements CharacterComparator {
+    private int _N;
+    public OffByN(int N) {
+        _N = N;
+    }
+
+    @Override
+    public boolean equalChars(char x, char y) {
+        return x - y == _N || y - x == _N;
+    }
+}
