d6ec8effe742fe7f1dcd8fd49b3dd87a44e0d667
johnnyZeppelin <936645698@qq.com>
2022-04-27 22:32:35 +0800

    Update with manually input OffByN

--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /** This class outputs all palindromes in the words file in the current directory. */
 public class PalindromeFinder {
     public static void main(String[] args) {
@@ -5,9 +7,12 @@ public class PalindromeFinder {
         In in = new In("../library-sp18/library-sp18/data/words.txt");
         Palindrome palindrome = new Palindrome();
 
+        System.out.println("Palindrome off by:");
+        int n = new Scanner(System.in).nextInt();
+        CharacterComparator cc = new OffByN(n);
         while (!in.isEmpty()) {
             String word = in.readString();
-            if (word.length() >= minLength && palindrome.isPalindrome(word)) {
+            if (word.length() >= minLength && palindrome.isPalindrome(word, cc)) {
                 System.out.println(word);
             }
         }
