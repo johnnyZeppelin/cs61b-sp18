37a5db2b04adbb0e486f10841e28f48152662919
johnnyZeppelin <936645698@qq.com>
2022-04-28 16:04:19 +0800

    Write the methods

--- xercises/src/List.java
+++ xercises/src/List.java
@@ -1,2 +1,12 @@
-public interface List {
+public interface List<T> {
+    void addFirst(T item);
+    void addLast(T item);
+    T removeFirst();
+    T removeLast();
+    void printDeque();
+    boolean isEmpty();
+    T get(int index);
+    default int size() {
+        return size();
+    }
 }
