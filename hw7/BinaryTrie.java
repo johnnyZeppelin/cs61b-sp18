11464d9906adace9861540b86174ab6aa5f9652d
johnnyZeppelin <936645698@qq.com>
2022-06-19 21:52:27 +0800

    finale seam

---
+++ hw7/BinaryTrie.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+import java.util.Map;
+
+public class BinaryTrie implements Serializable {
+    public BinaryTrie(Map<Character, Integer> frequencyTable) {
+
+    }
+    public Match longestPrefixMatch(BitSequence querySequence) {
+        // TODO: 2022-06-19
+        return null;
+    }
+    public Map<Character, BitSequence> buildLookupTable() {
+        // TODO: 2022-06-19
+        return null;
+    }
+
+}
